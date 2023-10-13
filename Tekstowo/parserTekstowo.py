2fbfa083848a913a41b59b8c1c8270e0172c0251
dprzygocka <przygocka.dagmara@gmail.com>
2023-10-13 21:08:44 +0200

    generate the files for missing and found songs

--- Tekstowo/parserTekstowo.py
+++ Tekstowo/parserTekstowo.py
@@ -18,9 +18,12 @@ except FileNotFoundError:
     sys.exit(1)
 
 data = {'item': []}
+notFoundData = {'item': []}
+written = False
 for song in songs['items']:
+    written = False
     url = 'https://www.tekstowo.pl/piosenka,'
-    a = [subitem.lower().replace(' ', '_').replace('.', '_').replace(')', '_').replace('-', '_').replace('(', '_').replace('ł', 'L')
+    a = [subitem.lower().replace(' ', '_').replace('.', '_').replace(')', '_').replace('-', '_').replace('(','_').replace('ł', 'L')
      for item in song['artists'] 
      for subitem in item.split(" I ")]
 
@@ -31,6 +34,7 @@ for song in songs['items']:
             permutations_list.extend(permutations(a, r))
         except Exception:
             continue
+
     for perm in permutations_list:
         url = 'https://www.tekstowo.pl/piosenka,'
         if len(perm) > 2:
@@ -38,14 +42,12 @@ for song in songs['items']:
         artists = re.sub(r'\s\([^)]*\)', '', "_ ".join(perm))
         name_remove_parentheses = re.sub(r'\s\([^)]*\)', '', song['name'])
         name = re.sub(r'[ .]|_+', '_', name_remove_parentheses).lower().replace(',', '')
-        #case ... is ___ should be _ {'name': 'Seniorita (Gorąca Krew)', 'artists': ['Pezet', 'NOON']} https://www.tekstowo.pl/piosenka,pezet_noon,seniorita_gor%C4%85ca_krew_.html
-        #there is this one: {'name': 'Tak Miało Być', 'artists': ['Molesta Ewenement', 'Jamal']} but we are missing feat Jamal
-        # {'name': 'Niedopowiedzenia (feat. Pezet)', 'artists': ['Czarny HIFI', 'Pezet']} Pezet in title not as author
-        # ___ is not made to _ {'name': 'Nie mamy skrzydeł', 'artists': ['Miuosh']}
-        # {'name': 'Nie Odejdę Stąd', 'artists': ['POE (Projekt Ostry Emade)']} (Projekt Ostry Emade) makes it unreadable
         artist_encoded = urllib.parse.quote(artists)
         name_encoded = urllib.parse.quote(name)
         url = f'{url}{artist_encoded},{name_encoded}.html'
+
+        print('url')
+        print(url)
 
         # Send an HTTP request to the URL
         try:
@@ -53,7 +55,6 @@ for song in songs['items']:
         except Exception:
             continue
         
-        print(response)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
             author_title = soup.find("h1", {"class": "strong"}).get_text().split(" - ")
@@ -63,13 +64,18 @@ for song in songs['items']:
             text = re.sub(pattern, '', soup.findAll("div", {"class": "inner-text"})[0].get_text(separator="<br/>")).replace("<br/>", '').split('\n')
             filtered_text_list = [item for item in text if item != '' and item != ']' and item != '[' and item != ':']
             data['item'].append({'name': author_title[1], 'artist': author_title[0], 'lyrics': filtered_text_list})
+            written = True
             break
         else:
             print(f'Failed to retrieve the webpage. Status code: {response.status_code}')
-            if permutations_list[-1] == perm:
-                data['item'].append({'name': song['name'], 'artist': song['artists'], 'lyrics': ''})
+    if not written:
+        notFoundData['item'].append({'name': song['name'], 'artist': song['artists'], 'lyrics': ''})
+
 filename = sys.argv[1].split("\\")[-1]
+data['item'].append({'nr of songs from original file': len(songs['items']), 'nr of songs from in current file': len(data['item'])})
+notFoundData['item'].append({'nr of songs from original file': len(songs['items']), 'nr of songs from in current file': len(notFoundData['item'])})
 with open(f'tekstowo_{filename}', 'w', encoding='utf-8') as json_output_file:
     json.dump(data, json_output_file, ensure_ascii=False, indent=4)
-
+with open(f'missing_tekstowo_{filename}', 'w', encoding='utf-8') as json_output_file:
+    json.dump(notFoundData, json_output_file, ensure_ascii=False, indent=4)
 print(f"Data written to tekstowo_{filename}")
